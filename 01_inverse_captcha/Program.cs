9f128d920e7f7f74a80cc9472e802762791a8234
Miguel Carballo Garcia <miguel.garcia@5minds.de>
2017-12-04 16:21:37 +0100

    :sparkles: Add solutions for day 1

---
+++ 01_inverse_captcha/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _01_inverse_captcha
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String content = args[0];
+            int result = 0;
+
+            // Part One
+
+            for (int i = 0; i < content.Length - 1; i++)
+            {
+                if(content[i] == content[i+1]) 
+                {
+                    result += Convert.ToInt32(content[i].ToString());
+                }
+            }
+            
+            if(content[content.Length - 1] == content[0])
+            {
+                result += Convert.ToInt32(content[0].ToString());
+            }
+            Console.WriteLine(result.ToString());
+
+            // Part TWo
+
+            result = 0;
+            int halfWay = content.Length / 2;    
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                if(i + halfWay >= content.Length)
+                {
+                    int foo = halfWay - (content.Length - i);
+                    if(content[i] == content[foo])
+                    {
+                        result += Convert.ToInt32(content[i].ToString());
+                    }
+                }
+                else
+                {
+                    if(content[i] == content[i+halfWay]) 
+                    {
+                        result += Convert.ToInt32(content[i].ToString());
+                    }
+                }
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# adventofcode_2017
-Repository for Advent of Code puzzles of 2017
