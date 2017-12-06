a7621cca8766c01e3a0a77cc18eecb04e88b414d
Miguel Carballo Garcia <miguel.garcia@5minds.de>
2017-12-06 15:49:43 +0100

    :sparkles: Add solution

---
+++ 05_a_maze_of_twisty_trampolines/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _05_a_maze_of_twisty_trampolines
+{
+    using System;
+    using System.IO;
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] content = File.ReadAllLines("./input.txt");
+            int[] step = new int[content.Length];
+            int jumps = 0;
+            int position = 0;
+
+            // part one
+            
+            for (int i = 0; i < content.Length; i++)
+            {
+                step[i] = Convert.ToInt32(content[i].ToString());
+            }
+
+            
+            while(position >= 0 && position < content.Length)
+            {
+                jumps++;
+                position += step[position]++;
+            }
+
+            Console.WriteLine(jumps);
+
+            // part two
+
+            jumps = 0;
+            position = 0;
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                step[i] = Convert.ToInt32(content[i].ToString());
+            }
+
+            while(position >= 0 && position < content.Length)
+            {
+                jumps++;
+                if(step[position] >= 3)
+                {
+                    position += step[position]--;
+                }
+                else
+                {
+                    position += step[position]++;
+                }
+            }
+
+            Console.WriteLine(jumps);
+        }
+    }
+}
