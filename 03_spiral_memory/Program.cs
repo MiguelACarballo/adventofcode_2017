2a6301b7a29e4bdd01fce3720b79fc047e6750ed
Miguel Carballo Garcia <miguel.garcia@5minds.de>
2017-12-06 10:07:25 +0100

    :sparkles: Add solution

---
+++ 03_spiral_memory/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03_spiral_memory
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 289326;
+
+            int square = (int)Math.Sqrt(input);
+            int center = (square - 1) / 2;
+            int steps = Math.Max(0, center - 1 + Math.Abs(center - input % square));
+
+            Console.WriteLine(steps);
+        }
+    }
+}
